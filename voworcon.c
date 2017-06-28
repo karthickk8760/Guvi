33627949917859da3f53f67d4ca2935cb1a12191
karthickk8760 <karthickk8760@users.noreply.github.com>
2017-06-28 16:15:27 +0530

    Create voworcon.c

---
+++ voworcon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ char a;
+ scanf("%c",&a);
+ if((a==A||a==E||a==I||a==O||a==U)||(a==a||a==i||a==o||a==u||a==e))
+ {
+  printf("vowel");
+ }
+ elseif((a<=A&&a>=Z)||(a<=a&&a>=z))
+ {
+  printf("consonant"):
+  }
+  return 0;
+  }
