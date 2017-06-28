2adcbfcd8d478fed16d6f3a34a7416605596d42b
karthickk8760 <karthickk8760@users.noreply.github.com>
2017-06-28 16:20:53 +0530

    Create alpornot.c

---
+++ alpornot.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ char a;
+ scanf("%c",&a);
+ if((a>=A&&a<=Z)||(a>=a&&a<=z))
+ {
+ printf("alphabet");
+ }
+ else
+ {
+ printf("not an alphabet");
+ }
+ return 0;
+ }
