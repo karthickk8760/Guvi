2ccfac90d34546372d94c162fdb01c6cb664248e
karthickk8760 <karthickk8760@users.noreply.github.com>
2017-06-30 16:04:44 +0530

    Create larorsm.c

---
+++ larorsm.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c;
+ scanf("%d%d%d",&a,&b,&c);
+ if((a>b)&&(a>c)
+ {
+  printf("%d is largest",a);
+  }
+  elseif(b>c)
+  {
+  printf("%d is largest",b);
+  }
+  else
+  {
+   printf("%d is largest",c);
+   }
+   }
+  
