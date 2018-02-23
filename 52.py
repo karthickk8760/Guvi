8b085b91d290eac3e3fa70e7807844ce443e7143
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-02-23 09:17:32 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,21 @@
+x=input()
+if(x==1):
+    print("one")
+elif(x==2):
+    print("two")
+elif(x==3):
+    print("three")
+elif(x==4):
+    print("four")
+elif(x==5):
+    print("five")
+elif(x==6):
+    print("six")
+elif(x==7):
+    print("seven")
+elif(x==8):
+    print("eight")
+elif(x==9):
+    print("nine")
+elif(x==10):
+    print("ten")
