64b46461911314af9874dc18485b2fa1560c5970
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-03-01 19:05:04 +0530

    Update h6.py

--- h6.py
+++ h6.py
@@ -1,13 +1,14 @@
- a=input()
+a=input()
     b=[]
     c=0
     for i in range(0,a):
         d=input()
         b.append(d)
     for i in range(0,len(b)):
-        for j in range(0,len(b)):
+        for j in range(i+1,len(b)):
             if(b[i]==b[j]):
+                print(b[i])
                 c=c+1
         if(c==1):
-            print(b[i])
-        c=0
+            break
+
