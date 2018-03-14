39bcea44c6d030dacd55b67bda8132aa0e96af9e
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-03-14 15:24:27 +0530

    Create 83.py

---
+++ beginner/83.py
@@ -0,0 +1,13 @@
+a,b,c=raw_input().split(" ")
+a=int(a);
+c=int(c);
+if(b=="/"):
+    print(a/c)
+elif(b=="%"):
+    print(a%c)
+elif(b=="+"):
+    print(a+c)
+elif(b=="-"):
+    print(a-c)
+elif(b=="*"):
+    print(a*c)
