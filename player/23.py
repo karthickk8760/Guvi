d52b9f51022b1a6598c54c21d5eb32b991d79fce
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-03-20 19:49:31 +0530

    Create 23.py

---
+++ player/23.py
@@ -0,0 +1,11 @@
+n=int(input())
+    k=int(input())
+    a=[]
+    for x in range(n):
+	   m=int(input())
+	   a.append(m)
+    for x in range(k):
+	   m=int(input())
+	   a.append(m)
+    print(max(a))
+    print(len(a))
