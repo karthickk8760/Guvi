31baab0966ee76d6c0deea6d3db717fda30ab9a4
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-03-20 16:14:41 +0530

    Update 5.py

--- player/5.py
+++ player/5.py
@@ -1,16 +1,16 @@
 def rom_to_int(string):
     table=[['M',1000],['CM',900],['D',500],['CD',400],['C',100],['XC',90],['L',50],['XL',40],['X',10],['IX',9],['V',5],['IV',4],['I',1]]
     returnint=0
-    for pair in table:
-    	continueyes=True
-        while continueyes:
-            if len(string)>=len(pair[0]):
-                if string[0:len(pair[0])]==pair[0]:
-                    returnint+=pair[1]
-                    string=string[len(pair[0]):]
-                else: continueyes=False
-            else: continueyes=False
+    for p in table:
+    	c=True
+        while c:
+            if len(string)>=len(p[0]):
+                if string[0:len(p[0])]==p[0]:
+                    returnint+=p[1]
+                    string=string[len(p[0]):]
+                else: c=False
+            else: c=False
 
     return returnint
-rom=input()
+rom=raw_input()
 print(rom_to_int(rom))
