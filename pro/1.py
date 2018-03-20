b92b80eafabeaf3b464923510aea8318bb969e2d
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-03-20 20:20:54 +0530

    Update 1.py

--- pro/1.py
+++ pro/1.py
@@ -1,1 +1,17 @@
-
+def longcomstr(l):
+	c=l[0]
+	s=''
+	n=len(c)
+	for i in range(1,len(l)):
+		for j in range(0,len(l) and n):
+			if c[j]!=l[i][j]:
+				break
+			s=s+c[j]
+	print(s)
+def main():
+	try:
+		strarr=raw_input()
+		l=strarr.split()
+		longcomstr(l)
+	except:
+		print('invalid')
