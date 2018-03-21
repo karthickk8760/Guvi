56abdafebbbccf9bb6750863b2a70919049a8115
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-03-21 19:16:14 +0530

    Create 3.py

---
+++ pro/3.py
@@ -0,0 +1,14 @@
+a=list(raw_input())
+    b=list(raw_input())
+    d=len(a)
+    e=len(b)
+    i=0
+    j=0
+    c=[]
+    while d>0:
+        if a[i]==b[j]:
+            c.append(a[i])
+        j=j+1
+        i=i+1
+        d=d-1
+    print(e-len(c))
