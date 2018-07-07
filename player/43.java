4b4441fbd701be0e4d521a4100784e85d11d2d50
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-07-07 15:29:43 +0530

    Create 43.java

---
+++ player/43.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class sortt{
+    public static void main(String aa[]){
+    Scanner sc=new Scanner(System.in);
+    String s=sc.next();
+    String s1=sc.next();
+    if(s.contains(s1))
+    {
+        System.out.println("yes");
+    }
+    else
+    {
+        System.out.println("no");
+    }
+}
+}
