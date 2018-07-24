61a7278888085cca220b476e189b0b4720f892d9
karthickk8760 <karthickk8760@users.noreply.github.com>
2018-07-24 15:40:54 +0530

    Create 79.py

---
+++ player/79.py
@@ -0,0 +1,11 @@
+a=int(input());
+b=[];
+for i in range(a):
+    b.append(int(input()));
+b.sort()
+dmax=b[1]-b[0];
+for i in range(0,len(b)):
+    for j in range(i,len(b)):
+        if (b[j]-b[i])>dmax:
+            dmax=b[j]-b[i];
+print(dmax);
