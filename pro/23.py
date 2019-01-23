17b2ccaa1b9e619e1131b20e4e08c3afde41a036
karthickk8760 <karthickk8760@users.noreply.github.com>
2019-01-23 15:31:57 +0530

    Create 23.py

---
+++ pro/23.py
@@ -0,0 +1,22 @@
+def main():
+	s=input()
+	st=list(s)
+	n,e,s,w=(0,1,2,3)
+	dir=n
+	x,y=0,0
+	for move in st:
+		if move=='L':
+			dir=(dir+1)%4
+		elif move=='R':
+			dir=(4+dir-1)%4
+		else :
+			if dir==n:
+				y+=1
+			elif dir==e:
+				x+=1
+			elif dir==w:
+				x-=1
+			elif dir==s:
+				y-=1
+	return (x==0 and y==0)
+main()
