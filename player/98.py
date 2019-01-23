f5dfdaf5177a5926f5b363bb822cdf8e55790b40
karthickk8760 <karthickk8760@users.noreply.github.com>
2019-01-23 14:12:14 +0530

    Create 98.py

---
+++ player/98.py
@@ -0,0 +1,16 @@
+n=int(input())
+k=int(input())
+li=[]
+sum=0
+while n!=0:
+    r=n%10
+    li.append(r)
+    n=n//10
+for i in range(k+1):
+    if i in li:
+        sum+=1
+        print(sum)
+if sum==k:
+    print("Yes")
+else:
+    print("No")
