549b5c78e80904f04ce909c52825b0cc4a250c83
karthickk8760 <karthickk8760@users.noreply.github.com>
2019-01-23 14:42:07 +0530

    Update 9.py

--- hunter/9.py
+++ hunter/9.py
@@ -1,8 +1,8 @@
-y=0
-while y==0:
+k=0
+while k==0:
   r=int(input("give your input in integer"))
   if (r>=0):
     print(r)
   else:
     print("invalid")
-  y=int(input("press 0 to continue"))
+  k=int(input("press 0 to continue"))
