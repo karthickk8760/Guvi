6932b68f95c8100ed2a07e8c82990571430260a6
karthickk8760 <karthickk8760@users.noreply.github.com>
2019-02-06 09:02:22 +0530

    Create 25.py

---
+++ pro/25.py
@@ -0,0 +1,16 @@
+def maxmain():
+	s=0
+	l=[]
+	max=-1
+	n=int(input())
+	for i in range(n):
+		l.append(int(input()))
+	for i in range(n-1):
+		if l[i]<l[i+1]:
+			s+=1
+		else:
+			continue
+		if max<s:
+			max=s
+	print(max)
+maxmain()
